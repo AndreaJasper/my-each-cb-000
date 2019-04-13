0474f52838f4be61c44402712be44a49c8dae2e6
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-04-13 21:55:10 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,12 @@
 def my_each(array) # put argument(s) here
   # code here
-  while
-    array = []
+  i = 0
+  while i < array.length
+    yield(array[i]
+    i += 1
   end
- def each &block
-   array.each &block
+ def 
+   array
  end
 end
+
