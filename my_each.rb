d2507e6c63f6c529ea0590513e7a2a440fe43ca9
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-04-13 21:57:05 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,11 +1,11 @@
-def my_each(array) # put argument(s) here
-  # code here
-  i = 0
-  while i < array.length
-    yield(array[i]
-  end
-   array
- end
+def my_each(array)
+	i = 0
+	while i < array.length
+		yield(array[i])
+		i += 1
+	end
+	array
+end
  collection = [1, 2, 3, 4]
 my_each(collection) do |num|
  	num
