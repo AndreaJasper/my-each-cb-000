219af009fa28507608e54b889a96f2cb0b6ddb1a
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-04-13 21:47:48 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,11 +1,10 @@
 def my_each(array) # put argument(s) here
   # code here
   while i <
-    yield (array[i])
-    i = i +1
+    yield my_each do
+      puts "#{array}"
   end
   array
   end
-  my_each do
-    puts "#{array}"
+
 end
